57a3a0b8c1ed6a4ae5be11f69bdabbc5031c5440
Maciej Pacula <maciej@flagstaff.ai>
2023-06-12 10:43:46 -0400

    Add sleep statements between Selenium actions, and retry the whole execution if failed

--- tests/integration/jupyter_driver.py
+++ tests/integration/jupyter_driver.py
@@ -26,12 +26,12 @@ def find_element_with_alternatives(driver, by, possible_values):
         except selenium.common.exceptions.NoSuchElementException:
             continue  # try next possible value
 
-    raise RuntimeError(f"No such element.Tried: " + ", ".join(possible_values))
+    raise RuntimeError(f"No such element. Tried: " + ", ".join(possible_values))
 
 
 def retry(runnable, max_retries):
     for attempt in range(max_retries):
-        print(f"Attempt {attempt}...")
+        print(f"Selenium execution attempt {attempt}...")
         try:
             return runnable()
         except Exception as e:
@@ -41,16 +41,21 @@ def retry(runnable, max_retries):
     raise RuntimeError(f"Execution failed despite {max_retries} retries")
 
 def run_notebook(driver, jupyter_url):
+    """S"""
     if '/tree' in jupyter_url:
         # Running Notebook 7
         driver.get(jupyter_url.replace("/tree?token=", "/notebooks/integration_tests.ipynb?factory=Notebook&token="))
     else:
         driver.get(jupyter_url.replace("?token=", "notebooks/integration_tests.ipynb?token="))
 
+    time.sleep(10)
+
     try:
         # For Jupyter Notebook 6.x
         driver.find_element(by=By.CSS_SELECTOR, value="#kernellink").click()
+        time.sleep(5)
         driver.find_element(by=By.CSS_SELECTOR, value="#restart_run_all").click()
+        time.sleep(5)
     except selenium.common.exceptions.NoSuchElementException:
         # For Jupyter Notebook 5.x
         find_element_with_alternatives(driver, by=By.CSS_SELECTOR,
@@ -58,20 +63,81 @@ def run_notebook(driver, jupyter_url):
                                            'button[data-jupyter-action="jupyter-notebook:confirm-restart-kernel-and-run-all-cells"]',
                                            "button[data-command='runmenu:restart-and-run-all']"
                                        ]).click()
+        time.sleep(5)
     # Confirm
     find_element_with_alternatives(driver, by=By.CSS_SELECTOR,
                                    possible_values=[".modal-dialog button.btn.btn-danger",
                                                     ".jp-Dialog-button.jp-mod-warn"]).click()
+    time.sleep(5)
 
 
 def run_lab(driver, jupyter_url):
     driver.get(jupyter_url.replace("/lab?token=", "/lab/tree/integration_tests.ipynb?token="))
 
+    time.sleep(10)
+
     # Restart and run all button
     driver.find_element(by=By.CSS_SELECTOR, value="button[data-command='runmenu:restart-and-run-all']").click()
+    time.sleep(5)
 
     # Confirm
     driver.find_element(by=By.CSS_SELECTOR, value=".jp-Dialog-button.jp-mod-warn").click()
+    time.sleep(5)
+
+
+def run_attempt(args, working_dir, reader, writer):
+    proc = subprocess.Popen(["jupyter", args.service, "--no-browser", args.notebook_path],
+                            stdout=writer,
+                            stderr=writer)
+
+    driver = None
+    success = False
+    try:
+        jupyter_url = None
+        while proc.poll() is None and jupyter_url is None:
+            line = reader.readline()
+            m = re.match(r'.*(http.*\?token=\w+).*', line)
+            if m is not None:
+                jupyter_url = m.group(1)
+
+            time.sleep(0.5)
+
+        output_path = os.path.join(working_dir, "integration_test.json")
+        if os.path.exists(output_path):
+            os.remove(output_path)
+            print(f"Deleted {output_path}")
+
+        print(f"URL: {jupyter_url}")
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.implicitly_wait(30.0)
+
+        if args.service == "notebook":
+            retry(lambda: run_notebook(driver, jupyter_url), args.retries)
+        elif args.service == "lab":
+            retry(lambda: run_lab(driver, jupyter_url), args.retries)
+        else:
+            raise ValueError(f"Unsupported service: {args.service}")
+
+        start_time = datetime.now()
+        timed_out = True
+        while (datetime.now() - start_time).total_seconds() < args.timeout:
+            if os.path.exists(output_path + ".done"):
+                timed_out = False
+                success = True
+                break
+
+            time.sleep(1)
+
+        if timed_out:
+            print("Execution timed out.")
+
+    finally:
+        proc.terminate()
+
+        if driver is not None:
+            driver.close()
+
+        return success
 
 
 def main():
@@ -82,63 +148,23 @@ def main():
     parser.add_argument("--timeout", type=int, default=300,
                         help="Timeout in seconds (max 300s) for the notebook to finish execution")
     parser.add_argument("--retries", type=int, default=5,
-                        help="Maximum number of ties to retry execution. The tests are flaky due to Selenium not "
+                        help="Maximum number of execution attempts. The tests are flaky due to Selenium not "
                              "being able to find UI elements or them not loading in time.")
     args = parser.parse_args()
 
     working_dir = os.path.dirname(args.notebook_path)
     filename = os.path.join(working_dir, "driver.log")
+
+    attempt = 0
+    success = False
     with io.open(filename, "w") as writer, io.open(filename, "r", 1) as reader:
-        proc = subprocess.Popen(["jupyter", args.service, "--no-browser", args.notebook_path],
-                                stdout=writer,
-                                stderr=writer)
+        while attempt < args.retries and not success:
+            print(f"Running attempt {attempt}...")
+            success = run_attempt(args, working_dir, reader, writer)
+            attempt += 1
 
-        driver = None
-        try:
-            jupyter_url = None
-            while proc.poll() is None and jupyter_url is None:
-                line = reader.readline()
-                m = re.match(r'.*(http.*\?token=\w+).*', line)
-                if m is not None:
-                    jupyter_url = m.group(1)
-
-                time.sleep(0.5)
-
-            output_path = os.path.join(working_dir, "integration_test.json")
-            if os.path.exists(output_path):
-                os.remove(output_path)
-                print(f"Deleted {output_path}")
-
-            print(f"URL: {jupyter_url}")
-            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-            driver.implicitly_wait(30.0)
-
-            if args.service == "notebook":
-                retry(lambda: run_notebook(driver, jupyter_url), args.retries)
-            elif args.service == "lab":
-                retry(lambda: run_lab(driver, jupyter_url), args.retries)
-            else:
-                raise ValueError(f"Unsupported service: {args.service}")
-
-            start_time = datetime.now()
-            timed_out = True
-            while (datetime.now() - start_time).total_seconds() < args.timeout:
-                if os.path.exists(output_path + ".done"):
-                    timed_out = False
-                    break
-
-                time.sleep(1)
-
-            if timed_out:
-                raise RuntimeError("Execution timed out.")
-
-            print(f"Finished after {(datetime.now() - start_time).total_seconds():.2f} seconds")
-
-        finally:
-            proc.terminate()
-
-            if driver is not None:
-                driver.close()
+    status = "Succeeded" if success else "Failed"
+    print(f"{status} after {attempt} attempts.")
 
 
 if __name__ == "__main__":
